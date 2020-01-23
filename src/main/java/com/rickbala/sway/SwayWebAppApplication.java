945f464e530a0f0b9cc77500e54a01c74cb7444e
rickbala <ricardovbalarini@gmail.com>
2020-01-22 21:49:07 -0300

    envars

--- src/main/java/com/rickbala/sway/SwayWebAppApplication.java
+++ src/main/java/com/rickbala/sway/SwayWebAppApplication.java
@@ -1,10 +1,14 @@
 package com.rickbala.sway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SwayWebAppApplication {
+
+	@Value("#{spring.data.mongodb.uri}")
+	private static String connectionUri;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwayWebAppApplication.class, args);
